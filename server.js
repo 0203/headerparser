5a65d8f099d57a3b2d727e0b71d5ad19c20a6296
Glitch (izraheaderparser) <none>
2018-03-26 08:40:58 +0000

    ⛷🏙 Checkpoint ./package.json:762202/3588 ./server.js:762202/1629

--- server.js
+++ server.js
@@ -3,7 +3,12 @@
 
 // init project
 const express = require('express')
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
 const app = express()
+app.use(bodyParser.json())
+app.use(cors())
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -11,27 +16,15 @@ const app = express()
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'))
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html')
+var api = '/api/whoami';
+
+app.get(api, function(request,response,next){
+  var language = request.acceptsLanguages();
+  var software;
+  var ipaddress = request.ip;
+  response.json({'ipaddress':ipaddress, 'language':language[0]});
 })
 
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
-
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
-})
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
